9f8704b023b472df5ff03f198a3d1430d533b573
Kenneth <deborah2020.kenneth@gmail.com>
2025-07-28 17:19:56 +0100

    Create a templates directory

--- ayce/urls.py
+++ ayce/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import render
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: render(request, 'home.html'), name='home'),
 ]
 
 if settings.DEBUG:
