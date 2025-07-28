23c68412c37348673aac201584d9bdcf8b05124c
Kenneth <deborah2020.kenneth@gmail.com>
2025-07-28 19:08:16 +0100

    Register menu in admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import MenuItem
 
 # Register your models here.
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'is_featured')
+    list_filter = ('is_featured',)
+    search_fields = ('name',)
--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+    is_featured = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
