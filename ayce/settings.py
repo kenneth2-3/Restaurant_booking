3c7d07bdac41dbe57f8c4eb0aeec013f0ad91531
Kenneth <deborah2020.kenneth@gmail.com>
2025-07-28 17:02:42 +0100

    Configure installed apps

--- ayce/settings.py
+++ ayce/settings.py
@@ -37,7 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookings',
+    'menu',
+    'accounts',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
