0e21e3860d496a8230c7ff56f853f894681f61d1
Kenneth <deborah2020.kenneth@gmail.com>
2025-07-28 19:50:49 +0100

    Create the menu to display items

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import MenuItem
 
 # Create your views here.
+
+def menu_list(request):
+    items = MenuItem.objects.all()
+    return render(request, 'menu/menu_list.html', {'items': items})
