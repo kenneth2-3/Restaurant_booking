56dddf839632d52a4455ac3b107f28156c18cf8e
Kenneth <deborah2020.kenneth@gmail.com>
2025-08-04 15:10:18 +0100

    Create the booking model

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    date = models.DateField()
+    time = models.TimeField()
+    guests = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.date} at {self.time}"
--- menu/views.py
+++ menu/views.py
@@ -3,6 +3,9 @@ from .models import MenuItem
 
 # Create your views here.
 
+def home(request):
+    return render(request, 'home.html')
+
 def menu_list(request):
     items = MenuItem.objects.all()
     return render(request, 'menu/menu_list.html', {'items': items})
