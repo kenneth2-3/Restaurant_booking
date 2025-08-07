0bed9d013ab379872702ef9b738a88599e73ac57
Kenneth <deborah2020.kenneth@gmail.com>
2025-08-07 17:06:46 +0100

    Create a cancellation success page

--- bookings/views.py
+++ bookings/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import BookingForm
+from django.http import HttpResponseNotFound
+from django.contrib import messages
+from .models import Booking
 
 # Create your views here.
 
@@ -12,3 +15,16 @@ def book_table(request):
     else:
         form = BookingForm()
     return render(request, 'bookings/book_table.html', {'form': form})
+
+def cancel_booking(request, token):
+    try:
+        booking = Booking.objects.get(cancel_token=token)
+        if booking.canceled:
+            messages.warning(request, "This booking was already cancelled.")
+        else:
+            booking.canceled = True
+            booking.save()
+            messages.success(request, "Your booking has been cancelled.")
+        return redirect('cancel_success')
+    except Booking.DoesNotExist:
+        return HttpResponseNotFound("Invalid cancellation link.")
