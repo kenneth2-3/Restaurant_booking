2617a236a96792bddfa8ebd94ef1e2bca2b1357f
Kenneth <deborah2020.kenneth@gmail.com>
2025-08-07 17:36:56 +0100

    Show cancel link after booking

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import book_table
+from django.shortcuts import render
+from .views import book_table, cancel_booking
 
 urlpatterns = [
     path('', book_table, name='book_table'),
+    path('cancel/<uuid:token>/', cancel_booking, name='cancel_booking'),
+    path('cancelled/', lambda r: render(r, 'bookings/cancel_success.html'), name='cancel_success'),
 ]
