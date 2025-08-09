ff1b436fe13edadd503eb7355f5f45835ee19d09
Kenneth <deborah2020.kenneth@gmail.com>
2025-08-09 13:39:38 +0100

    Create login templates and link booking to users

--- bookings/models.py
+++ bookings/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.utils import timezone
 import uuid
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=100)
     email = models.EmailField()
     date = models.DateField()
