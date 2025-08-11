fe6672966ad62cbc4c59a1720c287535083fa6b4
Kenneth <deborah2020.kenneth@gmail.com>
2025-08-11 18:09:50 +0100

    Create a logout success form

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login
-from .forms import RegisterForm
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
-def register_view(request):
+def register(request):
     if request.method == 'POST':
-        form = RegisterForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)
-            return redirect('home')
+            login(request, user)  # log in immediately after register
+            return redirect('home')  # change 'home' to your homepage URL name
     else:
-        form = RegisterForm()
+        form = UserCreationForm()
     return render(request, 'accounts/register.html', {'form': form})
