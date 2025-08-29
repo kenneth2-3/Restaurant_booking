458258c36c27c07a7ffb746a99e9e2618db892c7
Kenneth <deborah2020.kenneth@gmail.com>
2025-08-29 23:14:57 +0100

    Create a folder for holding screenshots of the tests

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,8 @@ from . import views
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
+app_name = "accounts"   
+
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('register/', views.register, name='register'),
--- menu/urls.py
+++ menu/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "menu"
 urlpatterns = [
     path('', views.home, name='home'),
     path('menu', views.menu_list, name='menu_list'),
