842cddb99154f54033e20a97ed1156def8521a8c
Kenneth <deborah2020.kenneth@gmail.com>
2025-09-01 03:08:08 +0100

    Update the settings

--- ayce/settings.py
+++ ayce/settings.py
@@ -125,15 +125,15 @@ WSGI_APPLICATION = 'ayce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
-#     'default': dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600)
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+DATABASES = {
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL'), conn_max_age=600)
+}
 
 
 # Password validation
