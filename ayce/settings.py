286707637babf5dccbfa9cc10b8ef3ba90d02dc9
Kenneth <deborah2020.kenneth@gmail.com>
2025-09-01 01:44:58 +0100

    Add images to menu section

--- ayce/settings.py
+++ ayce/settings.py
@@ -181,6 +181,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# Media files (uploads)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
